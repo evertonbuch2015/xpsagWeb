3e581886f888b4260cdb9fb087a07667bceea8d4
evertonbuch2015 <everton.buch@gmail.com>
2017-02-15 21:08:02 -0200

    Correção das classes clasFiscal e clasFiscalItem.

--- src/test/java/xpsagWeb/Teste.java
+++ src/test/java/xpsagWeb/Teste.java
@@ -1,12 +1,19 @@
 package xpsagWeb;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.rudar.DAO.JPAUtil;
+import br.com.rudar.entity.ClasFiscal;
+import br.com.rudar.entity.ClasFiscalItem;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		//RomaneioDao dao = new RomaneioDao();
 		//UsuarioDao usuarioDao = new UsuarioDao();
 		//ColaboradorDao colaboradorDao = new ColaboradorDao();
-	//	EntityManager em = JPAUtil.GetInstance().getEntityManager();
+		EntityManager em = JPAUtil.GetInstance().getEntityManager();
 		//Integer i =colaboradorDao.getMaxCodigo();
 		
 		/*List<TipoColaborador> lista = new ArrayList<>();
@@ -24,7 +31,52 @@ public class Teste {
 		
 //		Usuario t = usuarioDao.findOne("From Usuario c where c.idUsusario > ?1", 1);
 			
-		//mais um teste..
+		try {
+			
+			em.getTransaction().begin();
+			/*
+			ClasFiscal clasFiscal = new ClasFiscal();
+			
+			ClasFiscalItem clasFiscalItem = new ClasFiscalItem();
+			clasFiscalItem.setClasFiscal(clasFiscal);
+			clasFiscalItem.setEstado("AA");
+			
+			ClasFiscalItem clasFiscalItem2 = new ClasFiscalItem();
+			clasFiscalItem2.setClasFiscal(clasFiscal);
+			clasFiscalItem2.setEstado("BB");
+			
+			ClasFiscalItem clasFiscalItem3 = new ClasFiscalItem();
+			clasFiscalItem3.setClasFiscal(clasFiscal);
+			clasFiscalItem3.setEstado("CC");
+			
+			ClasFiscalItem clasFiscalItem4 = new ClasFiscalItem();
+			clasFiscalItem4.setClasFiscal(clasFiscal);
+			clasFiscalItem4.setEstado("DD");
+			
+			clasFiscal.getClasFiscalItems().add(clasFiscalItem);
+			clasFiscal.getClasFiscalItems().add(clasFiscalItem2);
+			clasFiscal.getClasFiscalItems().add(clasFiscalItem3);
+			clasFiscal.getClasFiscalItems().add(clasFiscalItem4);
+			
+			
+			em.persist(clasFiscal);
+			em.getTransaction().commit();*/
+			
+			//ClasFiscal clasFiscal = em.find(ClasFiscal.class, 32450);
+			
+			//Query query = em.createQuery("Select c From ClasFiscalItem c where c.clasFiscal = 32450");
+			Query query = em.createQuery("Select c From ClasFiscal c left join fetch c.clasFiscalItems where c.codCadclasfiscal = 32450",ClasFiscal.class);
+			ClasFiscal clasFiscal = (ClasFiscal) query.getSingleResult();
+			
+			for (ClasFiscalItem clasFiscalItem2 : clasFiscal.getClasFiscalItems()) {
+				System.out.println(clasFiscalItem2.getEstado());
+			}
+			
+			em.close();
+		
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
